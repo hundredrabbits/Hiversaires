69c150904f7ace4af710f5d9844c3c6b958a8a6b
Devine Lu Linvega <me@m0oo.com>
2013-02-10 15:46:55 +0900

    Connected Forest Documents

--- Dozenal Clock/DozenalViewController_WorldNode.m
+++ Dozenal Clock/DozenalViewController_WorldNode.m
@@ -17,12 +17,12 @@
 	node[2]		= [NSArray arrayWithObjects: @"3", @"0", @"1", @"0", nil];
 	node[3]		= [NSArray arrayWithObjects: @"11", @"10", @"2", @"4", nil];
 	node[4]		= [NSArray arrayWithObjects: @"5", @"3", @"0", @"0", nil];
-	node[5]		= [NSArray arrayWithObjects: @"6", @"0", @"4", @"0", nil];
+	node[5]		= [NSArray arrayWithObjects: @"6", @"0", @"4", @"doc3", nil];
 	node[6]		= [NSArray arrayWithObjects: @"0", @"7", @"5", @"0", nil];
-	node[7]		= [NSArray arrayWithObjects: @"0", @"8", @"0", @"6", nil];
+	node[7]		= [NSArray arrayWithObjects: @"0", @"8", @"act2", @"6", nil];
 	node[8]		= [NSArray arrayWithObjects: @"0", @"0", @"9", @"7", nil];
-	node[9]		= [NSArray arrayWithObjects: @"8", @"0", @"10", @"0", nil];
-	node[10]	= [NSArray arrayWithObjects: @"9", @"0", @"0", @"3", nil];
+	node[9]		= [NSArray arrayWithObjects: @"8", @"doc2", @"10", @"0", nil];
+	node[10]	= [NSArray arrayWithObjects: @"9", @"0", @"doc3", @"3", nil];
 	node[11]	= [NSArray arrayWithObjects: @"act1", @"0", @"3", @"0", nil];
 	
 	return node;
@@ -65,6 +65,8 @@
 	
 	document[0]		= [NSArray arrayWithObjects: @"First Puzzle", @"Text", @"Graphic", nil];
 	document[1]		= [NSArray arrayWithObjects: @"First Puzzle", @"Text", @"Graphic", nil];
+	document[2]		= [NSArray arrayWithObjects: @"First Puzzle", @"Text", @"Graphic", nil];
+	document[3]		= [NSArray arrayWithObjects: @"First Puzzle", @"Text", @"Graphic", nil];
 	
 	return document;
 }
