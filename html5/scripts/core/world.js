d1d3ab698cb403f752602c196644da50cb6a98f0
Rezmason <jeremysachs@rezmason.net>
2018-01-30 16:31:56 -0800

    World is now built from values returned from static methods.

--- html5/scripts/core/world.js
+++ html5/scripts/core/world.js
@@ -1,9 +1,17 @@
-function makeNode(op0, op1, op2, op3, zone) {
+function makeNode(zone, op0, op1, op2, op3) {
   return [op0, op1, op2, op3, zone];
 }
 
 function makePuzzle(type) {
   return type;
+}
+
+function to(node, orientation) {
+  return orientation != null ? node + "|" + orientation : node.toString();
+}
+
+function pz(puzzleID) {
+  return "act" + puzzleID;
 }
 
 class World {
@@ -15,146 +23,146 @@ class World {
       let node = this.nodesByID;
 
       for (let i = 0; i < 150; i++) {
-        node[i] = ["0", "0", "0", "0", Zone.forest];
+        node[i] = makeNode(Zone.forest, to(0), to(0), to(0), to(0));
       }
 
       // ====================
       // Forest ( 0 - 11 )
       // ====================
 
-      node[0] = makeNode("1", "0", "0", "0", Zone.forest);
-      node[1] = makeNode("2", "0", "act28", "0", Zone.forest);
+      node[0] = makeNode(Zone.forest, to(1), to(0), to(0), to(0));
+      node[1] = makeNode(Zone.forest, to(2), to(0), pz(28), to(0));
 
-      // node[1] = makeNode("46", "0", "0", "0", Zone.forest);
+      // node[1] = makeNode(Zone.forest, to(46), to(0), to(0), to(0));
 
-      node[2] = makeNode("3", "0", "1", "0", Zone.forest);
-      node[3] = makeNode("11", "10|2", "2", "4|0", Zone.forest);
-      node[4] = makeNode("5", "0", "3|1", "0", Zone.forest);
-      node[5] = makeNode("6", "0", "4", "act4", Zone.forest);
-      node[6] = makeNode("7|1", "0", "5", "0", Zone.forest);
-      node[7] = makeNode("12", "8|0", "act1", "6|2", Zone.forest);
-      node[8] = makeNode("9|2", "0", "7|3", "0", Zone.forest);
-      node[9] = makeNode("8|2", "0", "10|0", "0", Zone.forest);
+      node[2] = makeNode(Zone.forest, to(3), to(0), to(1), to(0));
+      node[3] = makeNode(Zone.forest, to(11), to(10, 2), to(2), to(4, 0));
+      node[4] = makeNode(Zone.forest, to(5), to(0), to(3, 1), to(0));
+      node[5] = makeNode(Zone.forest, to(6), to(0), to(4), pz(4));
+      node[6] = makeNode(Zone.forest, to(7, 1), to(0), to(5), to(0));
+      node[7] = makeNode(Zone.forest, to(12), to(8, 0), pz(1), to(6, 2));
+      node[8] = makeNode(Zone.forest, to(9, 2), to(0), to(7, 3), to(0));
+      node[9] = makeNode(Zone.forest, to(8, 2), to(0), to(10, 0), to(0));
 
-      node[10] = makeNode("3|3", "0", "9|0", "0", Zone.forest);
-      node[11] = makeNode("act25", "0", "3", "0", Zone.forest);
-      node[12] = makeNode("act3", "0", "7", "14|0", Zone.forest);
-      node[13] = makeNode("17", "15", "act3", "act2", Zone.studio);
-      node[14] = makeNode("18", "0", "12|1", "0", Zone.forest);
-      node[15] = makeNode("16", "0", "0", "13", Zone.studio);
-      node[16] = makeNode("21", "act7", "15", "17", Zone.studio);
-      node[17] = makeNode("20", "16", "13", "0", Zone.studio);
-      node[18] = makeNode("act31", "act2", "14", "0", Zone.forest);
-      node[19] = makeNode("0", "20", "act5", "0", Zone.studio);
+      node[10] = makeNode(Zone.forest, to(3, 3), to(0), to(9, 0), to(0));
+      node[11] = makeNode(Zone.forest, pz(25), to(0), to(3), to(0));
+      node[12] = makeNode(Zone.forest, pz(3), to(0), to(7), to(14, 0));
+      node[13] = makeNode(Zone.studio, to(17), to(15), pz(3), pz(2));
+      node[14] = makeNode(Zone.forest, to(18), to(0), to(12, 1), to(0));
+      node[15] = makeNode(Zone.studio, to(16), to(0), to(0), to(13));
+      node[16] = makeNode(Zone.studio, to(21), pz(7), to(15), to(17));
+      node[17] = makeNode(Zone.studio, to(20), to(16), to(13), to(0));
+      node[18] = makeNode(Zone.forest, pz(31), pz(2), to(14), to(0));
+      node[19] = makeNode(Zone.studio, to(0), to(20), pz(5), to(0));
 
-      node[20] = makeNode("act6", "21", "17", "19", Zone.studio);
-      node[21] = makeNode("act35", "0", "16", "20", Zone.studio);
-      node[22] = makeNode("0", "23", "0", "16", Zone.circular);
-      node[23] = makeNode("30", "act16", "24", "act7", Zone.circular);
-      node[24] = makeNode("23", "0", "25", "0", Zone.circular);
-      node[25] = makeNode("24", "0", "26", "act8", Zone.circular);
-      node[26] = makeNode("25", "0", "27", "0", Zone.circular);
-      node[27] = makeNode("26", "0", "28", "act9", Zone.circular);
-      node[28] = makeNode("27", "0", "29", "act40", Zone.circular);
-      node[29] = makeNode("28", "0", "30", "33|0", Zone.circular);
+      node[20] = makeNode(Zone.studio, pz(6), to(21), to(17), to(19));
+      node[21] = makeNode(Zone.studio, pz(35), to(0), to(16), to(20));
+      node[22] = makeNode(Zone.circular, to(0), to(23), to(0), to(16));
+      node[23] = makeNode(Zone.circular, to(30), pz(16), to(24), pz(7));
+      node[24] = makeNode(Zone.circular, to(23), to(0), to(25), to(0));
+      node[25] = makeNode(Zone.circular, to(24), to(0), to(26), pz(8));
+      node[26] = makeNode(Zone.circular, to(25), to(0), to(27), to(0));
+      node[27] = makeNode(Zone.circular, to(26), to(0), to(28), pz(9));
+      node[28] = makeNode(Zone.circular, to(27), to(0), to(29), pz(40));
+      node[29] = makeNode(Zone.circular, to(28), to(0), to(30), to(33, 0));
 
-      node[30] = makeNode("29", "0", "23", "0", Zone.circular);
-      node[31] = makeNode("25|1", "0", "35", "0", Zone.circular);
-      node[32] = makeNode("0", "52", "0", "27|1", Zone.studio);
-      node[33] = makeNode("act14", "0", "29|1", "0", Zone.circular);
-      node[34] = makeNode("act37", "0", "0", "39", Zone.entente);
-      node[35] = makeNode("act8", "38", "36", "0", Zone.stones);
-      node[36] = makeNode("35", "37", "0", "0", Zone.stones);
-      node[37] = makeNode("38", "39", "0", "36", Zone.stones);
-      node[38] = makeNode("act12", "0", "37", "35", Zone.stones);
-      node[39] = makeNode("act10", "act11", "40", "37", Zone.stones);
+      node[30] = makeNode(Zone.circular, to(29), to(0), to(23), to(0));
+      node[31] = makeNode(Zone.circular, to(25, 1), to(0), to(35), to(0));
+      node[32] = makeNode(Zone.studio, to(0), to(52), to(0), to(27, 1));
+      node[33] = makeNode(Zone.circular, pz(14), to(0), to(29, 1), to(0));
+      node[34] = makeNode(Zone.entente, pz(37), to(0), to(0), to(39));
+      node[35] = makeNode(Zone.stones, pz(8), to(38), to(36), to(0));
+      node[36] = makeNode(Zone.stones, to(35), to(37), to(0), to(0));
+      node[37] = makeNode(Zone.stones, to(38), to(39), to(0), to(36));
+      node[38] = makeNode(Zone.stones, pz(12), to(0), to(37), to(35));
+      node[39] = makeNode(Zone.stones, pz(10), pz(11), to(40), to(37));
 
-      node[40] = makeNode("39", "0", "41", "0", Zone.stones);
-      node[41] = makeNode("40", "0", "0", "42", Zone.stones);
-      node[42] = makeNode("0", "41", "44", "43", Zone.stones);
-      node[43] = makeNode("0", "42", "0", "act35", Zone.stones);
-      node[44] = makeNode("42", "0", "46", "0", Zone.stones);
-      node[45] = makeNode("act13", "0", "act1", "39", Zone.rainre);
-      node[46] = makeNode("44", "0", "act15", "0", Zone.stones);
-      node[47] = makeNode("0", "48", "77", "0", Zone.antechannel);
-      node[48] = makeNode("act25", "0", "49", "47", Zone.antechannel);
-      node[49] = makeNode("48", "0", "act21", "77", Zone.antechannel);
-      node[50] = makeNode("0", "0", "act41", "0", Zone.nataniev);
-      node[51] = makeNode("0", "0", "64", "0", Zone.nataniev);
+      node[40] = makeNode(Zone.stones, to(39), to(0), to(41), to(0));
+      node[41] = makeNode(Zone.stones, to(40), to(0), to(0), to(42));
+      node[42] = makeNode(Zone.stones, to(0), to(41), to(44), to(43));
+      node[43] = makeNode(Zone.stones, to(0), to(42), to(0), pz(35));
+      node[44] = makeNode(Zone.stones, to(42), to(0), to(46), to(0));
+      node[45] = makeNode(Zone.rainre, pz(13), to(0), pz(1), to(39));
+      node[46] = makeNode(Zone.stones, to(44), to(0), pz(15), to(0));
+      node[47] = makeNode(Zone.antechannel, to(0), to(48), to(77), to(0));
+      node[48] = makeNode(Zone.antechannel, pz(25), to(0), to(49), to(47));
+      node[49] = makeNode(Zone.antechannel, to(48), to(0), pz(21), to(77));
+      node[50] = makeNode(Zone.nataniev, to(0), to(0), pz(41), to(0));
+      node[51] = makeNode(Zone.nataniev, to(0), to(0), to(64), to(0));
 
-      node[52] = makeNode("54", "53", "0", "act9", Zone.antechannel);
-      node[53] = makeNode("55", "84", "0", "52", Zone.antechannel);
-      node[54] = makeNode("57", "55", "52", "0", Zone.antechannel);
-      node[55] = makeNode("act39", "56", "53", "54", Zone.antechannel);
-      node[56] = makeNode("58", "0", "84", "55", Zone.antechannel);
-      node[57] = makeNode("59", "0", "54", "0", Zone.antechannel);
-      node[58] = makeNode("60", "0", "56", "0", Zone.antechannel);
-      node[59] = makeNode("61", "0", "57", "0", Zone.antechannel);
+      node[52] = makeNode(Zone.antechannel, to(54), to(53), to(0), pz(9));
+      node[53] = makeNode(Zone.antechannel, to(55), to(84), to(0), to(52));
+      node[54] = makeNode(Zone.antechannel, to(57), to(55), to(52), to(0));
+      node[55] = makeNode(Zone.antechannel, pz(39), to(56), to(53), to(54));
+      node[56] = makeNode(Zone.antechannel, to(58), to(0), to(84), to(55));
+      node[57] = makeNode(Zone.antechannel, to(59), to(0), to(54), to(0));
+      node[58] = makeNode(Zone.antechannel, to(60), to(0), to(56), to(0));
+      node[59] = makeNode(Zone.antechannel, to(61), to(0), to(57), to(0));
 
-      node[60] = makeNode("62", "0", "56", "0", Zone.antechannel);
-      node[61] = makeNode("0", "0", "59", "act19", Zone.antechannel);
-      node[62] = makeNode("act26", "0", "60", "0", Zone.antechannel);
-      node[63] = makeNode("73", "69", "0", "67", Zone.metamondst);
-      node[64] = makeNode("63", "0", "0", "0", Zone.nataniev);
-      node[65] = makeNode("0", "0", "act46", "0", Zone.entente);
-      node[67] = makeNode("74", "63", "0", "70", Zone.metamondst);
-      node[69] = makeNode("act18", "act19", "0", "63", Zone.metamondst);
+      node[60] = makeNode(Zone.antechannel, to(62), to(0), to(56), to(0));
+      node[61] = makeNode(Zone.antechannel, to(0), to(0), to(59), pz(19));
+      node[62] = makeNode(Zone.antechannel, pz(26), to(0), to(60), to(0));
+      node[63] = makeNode(Zone.metamondst, to(73), to(69), to(0), to(67));
+      node[64] = makeNode(Zone.nataniev, to(63), to(0), to(0), to(0));
+      node[65] = makeNode(Zone.entente, to(0), to(0), pz(46), to(0));
+      node[67] = makeNode(Zone.metamondst, to(74), to(63), to(0), to(70));
+      node[69] = makeNode(Zone.metamondst, pz(18), pz(19), to(0), to(63));
 
-      node[70] = makeNode("75", "67", "0", "0", Zone.metamondst);
-      node[72] = makeNode("0", "61", "0", "69", Zone.metamondst);
-      node[73] = makeNode("81", "0", "63", "0", Zone.metamondst);
-      node[74] = makeNode("80", "0", "67", "0", Zone.metamondst);
-      node[75] = makeNode("76", "0", "70", "0", Zone.metamondst);
-      node[76] = makeNode("0", "0", "75", "act30", Zone.metamondst);
-      node[77] = makeNode("47", "49", "act26", "act27", Zone.antechannel);
-      node[78] = makeNode("83", "0", "0", "81", Zone.metamondst);
-      node[79] = makeNode("0", "act33", "87", "0", Zone.capsule);
+      node[70] = makeNode(Zone.metamondst, to(75), to(67), to(0), to(0));
+      node[72] = makeNode(Zone.metamondst, to(0), to(61), to(0), to(69));
+      node[73] = makeNode(Zone.metamondst, to(81), to(0), to(63), to(0));
+      node[74] = makeNode(Zone.metamondst, to(80), to(0), to(67), to(0));
+      node[75] = makeNode(Zone.metamondst, to(76), to(0), to(70), to(0));
+      node[76] = makeNode(Zone.metamondst, to(0), to(0), to(75), pz(30));
+      node[77] = makeNode(Zone.antechannel, to(47), to(49), pz(26), pz(27));
+      node[78] = makeNode(Zone.metamondst, to(83), to(0), to(0), to(81));
+      node[79] = makeNode(Zone.capsule, to(0), pz(33), to(87), to(0));
 
-      node[80] = makeNode("85", "0", "74", "0", Zone.metamondst);
-      node[81] = makeNode("82", "78", "73", "0", Zone.metamondst);
-      node[82] = makeNode("0", "83", "81", "act20", Zone.metamondst);
-      node[83] = makeNode("0", "0", "78", "82", Zone.metamondst);
-      node[84] = makeNode("56", "0", "act47", "53", Zone.antechannel);
-      node[85] = makeNode("act15", "0", "80", "86", Zone.metamondst);
-      node[86] = makeNode("0", "85", "0", "act1", Zone.metamondst);
-      node[87] = makeNode("79", "act30", "0", "88", Zone.capsule);
-      node[88] = makeNode("0", "87", "0", "141", Zone.capsule);
+      node[80] = makeNode(Zone.metamondst, to(85), to(0), to(74), to(0));
+      node[81] = makeNode(Zone.metamondst, to(82), to(78), to(73), to(0));
+      node[82] = makeNode(Zone.metamondst, to(0), to(83), to(81), pz(20));
+      node[83] = makeNode(Zone.metamondst, to(0), to(0), to(78), to(82));
+      node[84] = makeNode(Zone.antechannel, to(56), to(0), pz(47), to(53));
+      node[85] = makeNode(Zone.metamondst, pz(15), to(0), to(80), to(86));
+      node[86] = makeNode(Zone.metamondst, to(0), to(85), to(0), pz(1));
+      node[87] = makeNode(Zone.capsule, to(79), pz(30), to(0), to(88));
+      node[88] = makeNode(Zone.capsule, to(0), to(87), to(0), to(141));
 
-      node[89] = makeNode("act42", "0", "90", "0", Zone.entente);
-      node[90] = makeNode("89", "0", "91", "0", Zone.entente);
-      node[91] = makeNode("90", "act23", "103", "0", Zone.entente);
+      node[89] = makeNode(Zone.entente, pz(42), to(0), to(90), to(0));
+      node[90] = makeNode(Zone.entente, to(89), to(0), to(91), to(0));
+      node[91] = makeNode(Zone.entente, to(90), pz(23), to(103), to(0));
 
-      node[92] = makeNode("91", "0", "93", "0", Zone.entente);
-      node[93] = makeNode("92", "0", "94", "0", Zone.entente);
-      node[94] = makeNode("93", "0", "95", "0", Zone.entente);
-      node[95] = makeNode("94", "act24", "96", "0", Zone.entente);
-      node[96] = makeNode("95", "0", "97", "0", Zone.entente);
-      node[97] = makeNode("96", "0", "98", "0", Zone.entente);
-      node[98] = makeNode("97", "99", "65", "101", Zone.entente);
-      node[99] = makeNode("0", "100", "0", "98", Zone.entente);
-      node[100] = makeNode("0", "act44", "0", "99", Zone.entente);
-      node[101] = makeNode("0", "98", "act38", "102", Zone.entente);
-      node[102] = makeNode("0", "101", "0", "act45", Zone.entente);
-      node[103] = makeNode("91", "0", "act43", "0", Zone.entente);
+      node[92] = makeNode(Zone.entente, to(91), to(0), to(93), to(0));
+      node[93] = makeNode(Zone.entente, to(92), to(0), to(94), to(0));
+      node[94] = makeNode(Zone.entente, to(93), to(0), to(95), to(0));
+      node[95] = makeNode(Zone.entente, to(94), pz(24), to(96), to(0));
+      node[96] = makeNode(Zone.entente, to(95), to(0), to(97), to(0));
+      node[97] = makeNode(Zone.entente, to(96), to(0), to(98), to(0));
+      node[98] = makeNode(Zone.entente, to(97), to(99), to(65), to(101));
+      node[99] = makeNode(Zone.entente, to(0), to(100), to(0), to(98));
+      node[100] = makeNode(Zone.entente, to(0), pz(44), to(0), to(99));
+      node[101] = makeNode(Zone.entente, to(0), to(98), pz(38), to(102));
+      node[102] = makeNode(Zone.entente, to(0), to(101), to(0), pz(45));
+      node[103] = makeNode(Zone.entente, to(91), to(0), pz(43), to(0));
 
-      node[104] = makeNode("0", "100", "0", "105", Zone.entente);
-      node[105] = makeNode("0", "104", "act24", "106", Zone.entente);
-      node[106] = makeNode("0", "105", "0", "107", Zone.entente);
-      node[107] = makeNode("0", "106", "0", "108", Zone.entente);
-      node[108] = makeNode("0", "107", "0", "109", Zone.entente);
-      node[109] = makeNode("0", "108", "0", "110", Zone.entente);
-      node[110] = makeNode("0", "109", "0", "111", Zone.entente);
-      node[111] = makeNode("0", "110", "0", "1|0", Zone.entente);
+      node[104] = makeNode(Zone.entente, to(0), to(100), to(0), to(105));
+      node[105] = makeNode(Zone.entente, to(0), to(104), pz(24), to(106));
+      node[106] = makeNode(Zone.entente, to(0), to(105), to(0), to(107));
+      node[107] = makeNode(Zone.entente, to(0), to(106), to(0), to(108));
+      node[108] = makeNode(Zone.entente, to(0), to(107), to(0), to(109));
+      node[109] = makeNode(Zone.entente, to(0), to(108), to(0), to(110));
+      node[110] = makeNode(Zone.entente, to(0), to(109), to(0), to(111));
+      node[111] = makeNode(Zone.entente, to(0), to(110), to(0), to(1, 0));
 
-      node[112] = makeNode("115", "113", "0", "act33", Zone.nataniev);
-      node[113] = makeNode("114", "act36", "act40", "112", Zone.nataniev);
-      node[114] = makeNode("0", "0", "113", "115", Zone.nataniev);
-      node[115] = makeNode("0", "114", "112", "0", Zone.nataniev);
-      node[116] = makeNode("0", "0", "0", "20|2", Zone.entente);
+      node[112] = makeNode(Zone.nataniev, to(115), to(113), to(0), pz(33));
+      node[113] = makeNode(Zone.nataniev, to(114), pz(36), pz(40), to(112));
+      node[114] = makeNode(Zone.nataniev, to(0), to(0), to(113), to(115));
+      node[115] = makeNode(Zone.nataniev, to(0), to(114), to(112), to(0));
+      node[116] = makeNode(Zone.entente, to(0), to(0), to(0), to(20, 2));
 
-      node[141] = makeNode("0", "88", "0", "142", Zone.entente);
-      node[142] = makeNode("0", "141", "0", "act54", Zone.entente);
-      node[143] = makeNode("0", "act54", "0", "0", Zone.nataniev);
+      node[141] = makeNode(Zone.entente, to(0), to(88), to(0), to(142));
+      node[142] = makeNode(Zone.entente, to(0), to(141), to(0), pz(54));
+      node[143] = makeNode(Zone.nataniev, to(0), pz(54), to(0), to(0));
     }
 
     {
